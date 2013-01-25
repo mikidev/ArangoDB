64611c6565dba23986ecd56bbb3c5da33b2613b3
hkernbach <h.kernbach@triagens.de>
2013-01-25 18:21:25 +0100

    log changes

--- html/admin/js/views/logsView.js
+++ html/admin/js/views/logsView.js
@@ -2,9 +2,13 @@ var logsView = Backbone.View.extend({
   el: '#content',
   offset: 0,
   size: 10,
+  page: 1,
   table: 'logTableID',
+  totalAmount: 0,
+  totalPages: 0,
 
   initialize: function () {
+    this.totalAmount = this.collection.models[0].attributes.totalAmount;
   },
 
   events: {
@@ -12,37 +16,75 @@ var logsView = Backbone.View.extend({
     "click #error-switch" : "error",
     "click #warning-switch" : "warning",
     "click #debug-switch" : "debug",
-    "click #info-switch" : "info"
+    "click #info-switch" : "info",
+    "click #logTableID_first" : "firstTable",
+    "click #logTableID_last"  : "lastTable",
+    "click #logTableID_prev"  : "prevTable",
+    "click #logTableID_next"  : "nextTable",
+  },
+  firstTable: function () {
+    this.offset = 0;
+    this.clearTable();
+    this.collection.fillLocalStorage(this.table, this.offset, 10);
+    this.drawTable();
+  },
+  lastTable: function () {
+    this.totalPages = Math.ceil(this.totalAmount / 10);
+    this.offset = (this.totalPages * 10) - 10;
+    this.clearTable();
+    this.collection.fillLocalStorage(this.table, this.offset, 10);
+    this.drawTable();
+  },
+  prevTable: function () {
+    this.offset = this.offset - this.size;
+    this.clearTable();
+    this.collection.fillLocalStorage(this.table, this.offset, 10);
+    this.drawTable();
+  },
+  nextTable: function () {
+    this.offset = this.offset + this.size;
+    this.clearTable();
+    this.collection.fillLocalStorage(this.table, this.offset, 10);
+    this.drawTable();
   },
   all: function () {
+    this.resetState();
     this.table = "logTableID";
     this.clearTable();
-    this.collection.fillLocalStorage(this.table, 0, 10);
+    this.collection.fillLocalStorage(this.table, this.offset, 10);
     this.drawTable();
   },
   error: function() {
+    this.resetState();
     this.table = "critTableID";
     this.clearTable();
-    this.collection.fillLocalStorage(this.table, 0, 10);
+    this.collection.fillLocalStorage(this.table, this.offset, 10);
     this.drawTable();
   },
   warning: function() {
+    this.resetState();
     this.table = "warnTableID";
     this.clearTable();
-    this.collection.fillLocalStorage(this.table, 0, 10);
+    this.collection.fillLocalStorage(this.table, this.offset, 10);
     this.drawTable();
   },
   debug: function() {
+    this.resetState();
     this.table = "debugTableID";
     this.clearTable();
     this.collection.fillLocalStorage(this.table, 0, 10);
     this.drawTable();
   },
   info: function() {
+    this.resetState();
     this.table = "infoTableID";
     this.clearTable();
     this.collection.fillLocalStorage(this.table, 0, 10);
     this.drawTable();
+  },
+  resetState: function () {
+    this.offset = 0;
+    this.size = 10;
   },
 
   tabs: function () {
